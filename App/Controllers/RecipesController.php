9a6743ba214fe4d25cc9394279ada90fe3b6f768
Tomáš Hurtoš <hurtos.tom@gmail.com>
2026-01-20 15:42:05 +0100

    Pridaný model Recipe | View pre recepty | Upravená funkcia index v RecipeController

--- App/Controllers/RecipesController.php
+++ App/Controllers/RecipesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\Recipe;
 use Framework\Core\BaseController;
 use Framework\Http\Request;
 use Framework\Http\Responses\Response;
@@ -10,6 +11,21 @@ class RecipesController extends BaseController
 {
     public function index(Request $request): Response
     {
-        return $this->html();
+        $user = $this->app->getAuthenticator()->getUser();
+
+        if ($user->isLoggedIn()) {
+            $recipes = Recipe::getAll(
+                '(`is_public` = 1 OR `user_id` = ?)',
+                [$user->getId()],
+                orderBy: '`created_at` DESC'
+            );
+        } else {
+            $recipes = Recipe::getAll(
+                '`is_public` = 1',
+                [],
+                orderBy: '`created_at` DESC'
+            );
+        }
+        return $this->html(compact('recipes'));
     }
 }
